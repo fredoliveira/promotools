374cd27f484345e67b3d352b017e8b77512b6cd9
Fred Oliveira <fred@webreakstuff.com>
2012-04-26 19:51:15 +0100

    Removed test data, added license file

--- beatport/discotexas.rb
+++ beatport/discotexas.rb
@@ -2,16 +2,15 @@ require 'chart'
 require 'beatport'
 
 # things to look for
-artists = ["Xinobi", "Moullinex", "Lazydisco", "Sean Finn", "Azari & III"]
-label = ["Discotexas", "LoudBit"]
+artists = ["Xinobi", "Moullinex", "Lazydisco"]
+label = ["Discotexas"]
 
 # init
 beatport = Beatport.new
 chart = Chart.new(artists, label)
 
 # run
-output = ""
-output += "House chart:\n\n"
+output = "House chart:\n\n"
 output += chart.parse(beatport.get_house)
 output += "\nNu-disco chart:\n\n"
 output += chart.parse(beatport.get_nudisco)
@@ -26,8 +25,8 @@ def email_results(results)
     :enable_starttls_auto => config["config"]["tls"],
     :user_name            => config["config"]["username"],
     :password             => config["config"]["password"],
-    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain               => config["config"]["domain"] # the HELO domain provided by the client to the server
+    :authentication       => :plain,
+    :domain               => config["config"]["domain"]
   })
 end
 
